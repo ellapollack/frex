1ca06c9f0e56a9c22f0f13df6b9f985090e4c6f0
Maxwell Pollack <mail@maxis.cool>
2020-03-16 13:31:06 -0400

    Added call to parent constructor in AudioWorkletNode

--- tanhiir.js
+++ tanhiir.js
@@ -1,14 +1,16 @@
 class TanhIIRProcessor extends AudioWorkletProcessor {
+  constructor() { super(); }
+
   process (inputs, outputs, parameters) {
     let offset = 0;
     output.forEach(channel => {
       for (let i = 0; i < channel.length; i++) {
-        channel[i] = tanh(inputs[0][i]+offset);
+        channel[i] = tanh(inputs[0][i] + offset);
         offset = inputs[0][i] - channel[i];
       }
-    })
-    return true
+    });
+    return true;
   }
 }
 
-registerProcessor('tanh-iir-processor', TanhIIRProcessor)
+registerProcessor('tanh-iir-processor', TanhIIRProcessor);
