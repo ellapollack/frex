a79eaa2fc80fc39c68417d6181f85d311cc5eb28
Maxwell Pollack <mail@maxis.cool>
2020-03-16 15:47:28 -0400

    Fixed AudioWorkletProcessor process method

--- tanhiir.js
+++ tanhiir.js
@@ -2,10 +2,11 @@ class TanhIIRProcessor extends AudioWorkletProcessor {
   constructor() { super(); this.offset=0; }
 
   process (inputs, outputs, parameters) {
-
-    for (let i=0; i<inputs[0].length; i++) {
-      outputs.forEach(channel => {channel[i] = Math.tanh(inputs[0][i] + this.offset);});
-      this.offset = inputs[0][i] - outputs[0][i];
+    let input = inputs[0];
+    let output = outputs[0];
+    for (let i=0; i<input[0].length; i++) {
+      output.forEach(channel => {channel[i] = Math.tanh(input[0][i] + this.offset);});
+      this.offset = input[0][i] - output[0][i];
     }
 
     return true;
