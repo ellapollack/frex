696ac2329ade8f2d4d2a1b2328f25d2b2fd4ba3a
Maxwell Pollack <mail@maxis.cool>
2020-03-16 13:53:35 -0400

    Typo

--- tanhiir.js
+++ tanhiir.js
@@ -3,7 +3,7 @@ class TanhIIRProcessor extends AudioWorkletProcessor {
 
   process (inputs, outputs, parameters) {
     let offset = 0;
-    output.forEach(channel => {
+    outputs.forEach(channel => {
       for (let i = 0; i < channel.length; i++) {
         channel[i] = tanh(inputs[0][i] + offset);
         offset = inputs[0][i] - channel[i];
