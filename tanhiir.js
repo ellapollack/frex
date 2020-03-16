2fae51dffe30aac9005ea04ebbb8fdee547c0fd9
Maxwell Pollack <mail@maxis.cool>
2020-03-16 14:41:37 -0400

    Renamed variable

--- tanhiir.js
+++ tanhiir.js
@@ -1,13 +1,13 @@
 class TanhIIRProcessor extends AudioWorkletProcessor {
-  constructor() { super(); this._offset=0; }
+  constructor() { super(); this.offset=0; }
 
   process (inputs, outputs, parameters) {
 
     for (let i=0; i<inputs[0].length; i++) {
-      outputs.forEach(channel => {channel[i] = Math.tanh(inputs[0][i] + this._offset);});
-      this._offset = inputs[0][i] - channel[i];
+      outputs.forEach(channel => {channel[i] = Math.tanh(inputs[0][i] + this.offset);});
+      this.offset = inputs[0][i] - channel[i];
     }
-    
+
     return true;
   }
 }
